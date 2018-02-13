bb8dce71cf59c06e8e7414c865c501549370b2ab
Apurva N. Saraogi <apurva9@iitg.ernet.in>
2018-02-13 16:45:53 +0530

    Updated Algorithm

--- algorithms.py
+++ algorithms.py
@@ -5,16 +5,36 @@ import os
 def did_you_mean():
 	print ("DID YOU MEAN XYZ")
 
-def multiple_words(table, listofwords):
+def result(table,query):
+	x = time.time()
+	answer = intersect_file(table,query)
+	y = time.time()
+	print ("These are the results found. Time Taken: " + str(y-x) + " seconds.")
+	for dkey in answer:
+		print ("Filename: " + dkey)
+		print (answer[dkey])
+	
+	
+
+def intersect_file(table,query):
+	listofwords = query.split()
 	try:
-		x = time.time()
-		intersect = set(table[listofwords[0].lower()])
-		
+		books = set(table[listofwords[0].lower()].keys())		
 		for i in range(1,len(listofwords)):
-			intersect = intersect & set(table[listofwords[i].lower()])
-		y = time.time()
-		print (y-x)
-		return intersect
+			books = books & set(table[listofwords[i].lower()].keys())
+		return multiple_words(table,list(books),listofwords)
+	except KeyError:
+		did_you_mean()
+
+def multiple_words(table,list_books,listofwords):
+	answer = {}
+	try:
+		for book in list_books:
+			intersect = set(table[listofwords[0].lower()][book])		
+			for i in range(1,len(listofwords)):
+				intersect = intersect & set(table[listofwords[i].lower()][book])
+			answer[book] = list(intersect)
+		return answer
 	except KeyError:
 		did_you_mean()
 
@@ -25,3 +45,13 @@ def search_keys(table,query):
 		if myre.match(key):
 			result.append(key)
 	return result
+
+'''''''''''''''''''''''''''''''''''''''''''''
+Think for a second
+a dict -> word -> book -> page number
+2 method
+1. find out list of common books for all words, now after that take intersection of books
+now for these books in the list search the list
+Number of books O(n)*O(n)
+
+'''
